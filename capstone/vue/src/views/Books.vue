1aa9fe2a24fdf661ba3cc6d026bb9388c0755d87
William Mowry <mowryw1@gmail.com>
2023-08-07 16:23:35 -0400

    wm adding books/family views

---
+++ capstone/vue/src/views/Books.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="books">
+    <h1>Books Page</h1>
+    <p>You must be authenticated to see this</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "books"
+};
+</script>
