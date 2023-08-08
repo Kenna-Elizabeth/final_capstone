dc5c19b40a2faafab0da5d6048a4c0b7e309a61d
Kenna Elizabeth <kennaelizabethcan@gmail.com>
2023-08-08 12:51:33 -0400

    change nav elements to ul and li and add flex properties

--- capstone/vue/src/views/NavigationBar.vue
+++ capstone/vue/src/views/NavigationBar.vue
@@ -1,21 +1,41 @@
 <template>
-<div id="nav">
-      <img src="../assets/valley-libraries-logo.png">
-      <router-link v-bind:to="{ name: 'books' }">Books</router-link> | 
-      <router-link v-bind:to="{ name: 'family' }">Family</router-link>
-      <span v-if="$store.state.token != ''"> | </span>
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link> 
-    
-    </div>
-  
+  <div id="nav">
+    <li><img src="../assets/valley-libraries-logo.png" /></li>
+    <ul>
+      <li><router-link v-bind:to="{ name: 'books' }">Books</router-link> | </li>
+      <li><router-link v-bind:to="{ name: 'family' }">Family</router-link></li>
+      <li v-if="$store.state.token != ''"> | </li>
+      <li>
+        <router-link
+          v-bind:to="{ name: 'logout' }"
+          v-if="$store.state.token != ''">Logout</router-link
+        >
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
+#nav {
+  list-style-type: none;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+}
 
+ul {
+  list-style-type: none;
+  display: flex;
+  justify-content: space-between;
+}
+
+li {
+  display: flex;
+  justify-content: space-between;
+}
 </style>
