5819223d5d2e0dbd6eb112f2dbbc4d7ff02c2bd0
Nick Amstutz <nick.amstutz@gmail.com>
2023-08-11 16:03:56 -0400

    NA Additional test data

--- capstone/java/database/data.sql
+++ capstone/java/database/data.sql
@@ -7,14 +7,15 @@ INSERT INTO users (username,password_hash,role,family_id) VALUES ('child','$2a$0
 
 INSERT INTO books (family_id, isbn, title, author, cover_url, note) VALUES (1, '9781784877989', 'Kafka on the Shore', 'Haruki Murakami', 'https://covers.openlibrary.org/b/isbn/9781784877989-M.jpg', 'I liked this one!');
 INSERT INTO books (family_id, isbn, title, author, cover_url, note) VALUES (1, '9780356500157', 'The Girl With All the Gifts', 'M.R. Carey', 'https://covers.openlibrary.org/b/isbn/9780356500157-M.jpg', 'Could be interesting');
+INSERT INTO books (family_id, isbn, title, author, cover_url, note) VALUES (1, '9780684830490', 'The Old Man and the Sea', 'Ernest Hemingway', 'https://covers.openlibrary.org/b/isbn/9780684830490-M.jpg', '');
 
 INSERT INTO users_books (user_id, book_id, completed, recommended) VALUES (1, 1, true, false);
 INSERT INTO users_books (user_id, book_id, completed, recommended) VALUES (1, 2, false, true);
-INSERT INTO users_books (user_id, book_id, completed, recommended) VALUES (2, 1, false, false);
-INSERT INTO users_books (user_id, book_id, completed, recommended) VALUES (2, 2, false, false);
 
 INSERT INTO sessions (user_id, book_id, minutes, format, note) VALUES (1, 1, 120, 'Paper', '');
 INSERT INTO sessions (user_id, book_id, minutes, format, note) VALUES (1, 1, 180, 'Paper', 'Couldn''t put it down!');
+INSERT INTO sessions (user_id, book_id, minutes, format, note) VALUES (1, 3, 60, 'Audiobook', '');
+INSERT INTO sessions (user_id, book_id, minutes, format, note) VALUES (1, 3, 30, 'Audiobook', '');
 INSERT INTO sessions (user_id, book_id, minutes, format, note) VALUES (2, 2, 60, 'Read-Aloud (Listener)', '');
 
 COMMIT TRANSACTION;
