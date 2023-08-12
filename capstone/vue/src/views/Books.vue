3116126617dd3ddbf90c85134c464875e8da39d7
William Mowry <mowryw1@gmail.com>
2023-08-12 13:02:13 -0400

    updates to book panel

--- capstone/vue/src/views/Books.vue
+++ capstone/vue/src/views/Books.vue
@@ -3,17 +3,19 @@
     <div class="box">
       <h1>Books</h1>
     </div>
-      <section id="book-display">
+      <div id="book-display">
         <div class="book-panel" v-for="book in $store.state.books" :key="book.id">
-            <p>{{ book.title }}</p> 
-            <div id="book-image">
+            <div class="book-title">
+              {{ book.title }}
+            </div>
+            <div class="book-image">
               <img :src="book.coverUrl" />
             </div>
-            <div id="author">
-             <p>Author: {{ book.author }}</p> 
+            <div class="author">
+             {{ book.author }}
             </div>
         </div>
-      </section>
+      </div>
   </main>
 </template>
 
@@ -33,34 +35,40 @@ export default {
   display: flex; 
   justify-content: space-evenly;
   flex-wrap: wrap; 
-  
 }
+
 .book-panel {
   border: 2px solid black;
   border-radius: 8px;
   margin: 1em;
-  padding: 0.2em; 
+  padding: 0.2em;
   text-align: center;
   background-color: #eef2f3;
   box-shadow: 2px 3px #c9c9c9;
-  
+  font-weight: bold;
+  width: 14em;
+  height: auto;  
 }
 
-.book-panel:hover{
+.book-panel:hover {
   transform: scale(1.05);
   box-shadow: 5px 7px #c9c9c9;
 }
 
-#book-display  p {
-  font-weight: bold;
-}
-
 img {
-  height: 20em;
+  height: 18em;
 }
 
-#author {
+.book-title{
+  margin: 1em;
+  height: 1.5em;
   font-size: 1em;
+}
+
+.author {
+  font-size: .75em;
+  margin: 0.5em;
+  
 }
 
 
